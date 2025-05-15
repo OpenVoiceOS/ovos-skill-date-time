578a60f1952b492dadec101fac42e18f53425f8e
miro <jarbasai@mailfence.com>
2025-05-15 17:46:20 +0100

    fix: standardize urls / skill-id / pypi-name

--- test/unittests/test_skill_loading.py
+++ test/unittests/test_skill_loading.py
@@ -10,7 +10,7 @@ from skill_ovos_date_time import TimeSkill
 class TestSkillLoading(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.skill_id = "skill-ovos-date-time.openvoiceos"
+        self.skill_id = "ovos-skill-date-time.openvoiceos"
         self.path = dirname(dirname(dirname(__file__)))
 
     def test_from_class(self):
