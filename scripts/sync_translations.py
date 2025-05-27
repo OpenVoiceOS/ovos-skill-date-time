72c522516d59f76d01ccef69c0a0c50cc5439ea4
miro <jarbasai@mailfence.com>
2025-05-27 18:26:23 +0100

    fix: add training data for entity extractors

--- scripts/sync_translations.py
+++ scripts/sync_translations.py
@@ -12,10 +12,27 @@ locale = f"{dirname(dirname(__file__))}/locale"
 tx = f"{dirname(dirname(__file__))}/translations"
 
 for lang in os.listdir(tx):
+    entities = f"{tx}/{lang}/entities.json"
     intents = f"{tx}/{lang}/intents.json"
     dialogs = f"{tx}/{lang}/dialogs.json"
     vocs = f"{tx}/{lang}/vocabs.json"
     regexes = f"{tx}/{lang}/regexes.json"
+
+    if os.path.isfile(entities):
+        with open(entities) as f:
+            data = json.load(f)
+        for fid, samples in data.items():
+            if samples:
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
+                if fid.startswith("/"):
+                    p = f"{locale}/{lang.lower()}{fid}"
+                else:
+                    p = f"{locale}/{lang.lower()}/{fid}"
+                os.makedirs(os.path.dirname(p), exist_ok=True)
+                with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
+                    f.write("\n".join(sorted(samples)))
 
     if os.path.isfile(intents):
         with open(intents) as f:
